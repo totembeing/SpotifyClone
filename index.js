6c42ef1f23f27aec261393b25ef11bd6ecc39427
totembeing <109453460+totembeing@users.noreply.github.com>
2024-01-04 20:26:10 +0530

    Completed the upper left section along with functionality and started working on lower left section

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".home").on("click", function() {
+    const homePageUrl = "./index.html";
+    window.location.href = homePageUrl;
+})
+
+$(".search").on("click", function() {
+    const searchPageUrl = "./search.html";
+    window.location.href = searchPageUrl;
+})
