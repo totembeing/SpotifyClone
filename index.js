aca34fd98fabae40e53adb25c545a711e78c34e6
totembeing <109453460+totembeing@users.noreply.github.com>
2024-03-14 14:03:10 +0530

    Completed the upper part of Your Library section

--- index.js
+++ index.js
@@ -33,3 +33,65 @@ $("#searchTitle > a").mouseenter(function () {
 })
 
 //--------------------------------------------------------------------------
+
+
+//Your Library Hover Effect ------------------------------------------------
+
+$("#libraryIcon > ion-icon").mouseleave(function () {
+    $("#libraryIcon > ion-icon").css("color","rgb(65, 65, 65)");
+    $("#libraryIcon > ion-icon").css("transition","200ns ease-in-out");
+
+    $("#libraryTitle > p").css("color", "rgb(65, 65, 65)");
+    $("#libraryTitle > p").css("transition","200ns ease-in-out");
+})
+
+$("#libraryTitle > p").mouseleave(function () {
+    $("#libraryIcon > ion-icon").css("color","rgb(65, 65, 65)");
+    $("#libraryIcon > ion-icon").css("transition","200ns ease-in-out");
+
+    $("#libraryTitle > p").css("color", "rgb(65, 65, 65)");
+    $("#libraryTitle > p").css("transition","200ns ease-in-out");
+})
+
+$("#libraryIcon > ion-icon").mouseenter(function () {
+    $("#libraryIcon > ion-icon").css("color", "white");
+    $("#libraryIcon > ion-icon").css("transition","200ns ease-in-out");
+
+    $("#libraryTitle > p").css("color", "white");
+    $("#libraryTitle > p").css("transition","200ns ease-in-out");
+})
+
+$("#libraryTitle > p").mouseenter(function () {
+    $("#libraryIcon > ion-icon").css("color", "white");
+    $("#libraryIcon > ion-icon").css("transition","200ns ease-in-out");
+
+    $("#libraryTitle > p").css("color", "white");
+    $("#libraryTitle > p").css("transition","200ns ease-in-out");
+})
+
+$("#showMoreIcon").mouseleave(function () {
+    $("#showMoreIcon").css("color", "rgb(65, 65, 65)");
+    $("#showMoreIcon").css("background-color", "rgb(18, 18, 18)");
+    $("#showMoreIcon").css("transition", "200ms ease-in-out");
+})
+
+$("#showMoreIcon").mouseenter(function () {
+    $("#showMoreIcon").css("color", "white");
+    $("#showMoreIcon").css("background-color", "rgb(25, 25, 25)");
+    $("#showMoreIcon").css("transition", "200ms ease-in-out");
+})
+
+$("#createIcon").mouseleave(function () {
+    $("#createIcon").css("color", "rgb(65, 65, 65)");
+    $("#createIcon").css("background-color", "rgb(18, 18, 18)");
+    $("#createIcon").css("transition", "200ms ease-in-out");
+})
+
+
+$("#createIcon").mouseenter(function () {
+    $("#createIcon").css("color", "white");
+    $("#createIcon").css("background-color", "rgb(25, 25, 25)");
+    $("#createIcon").css("transition", "200ms ease-in-out");
+})
+
+//--------------------------------------------------------------------------
